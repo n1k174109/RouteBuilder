0b872821cc213bbf0c730b4ebd14fc6b7795bfeb
n1k174109 <nikita2345652@gmail.com>
2021-06-30 13:46:25 +0300

    /

--- src/main/java/GraphWay.java
+++ src/main/java/GraphWay.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GraphWay {
-    private long id;
+//    private long id;
+    private Map<Long, List<Long>> ways;
 
-    public GraphWay(long id) {
-        this.id = id;
+    public GraphWay(Map<Long, List<Long>> ways) {
+        this.ways = ways;
     }
+//    public long getID() {
+//        return id;
+//    }
 
-    public long getID() {
-        return id;
-    }
+    public Map<Long, List<Long>> getWays()              { return ways; }
+    public void setWays(Map<Long, List<Long>> ways) { this.ways = ways; }
 }
--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -15,26 +15,16 @@ import java.util.*;
 
 public class NewMain {
     private List<GraphNode> nodes = new ArrayList<>();
-    private List<GraphWay> ways = new ArrayList<>();
-    private Set<Long> refs = new HashSet<>();
-    private int nodeElem = 0, wayElem = 0, relElem = 0;
-//    private Map<GraphWay, Integer> ways= new HashMap<>();
+//    private List<GraphWay> ways = new ArrayList<>();
+    private List<Long> refs = new ArrayList<>();
+    private Map<Long, List<Long>> ways = new HashMap<>();
+    private int nodeElem = 0;
 
-
-
-    public void setElements(int nodeElem, int wayElem, int relElem) {
+    public void setNodeElem(int nodeElem) {
         this.nodeElem = nodeElem;
-        this.wayElem = wayElem;
-        this.relElem = relElem;
     }
     public int getNodeElem() {
         return nodeElem;
-    }
-    public int getWayElem() {
-        return wayElem;
-    }
-    public int getRelElem() {
-        return relElem;
     }
 
     public NewMain() {}
@@ -42,7 +32,8 @@ public class NewMain {
         new NewMain().run();
     }
     public void run() throws ParserConfigurationException, IOException, SAXException {
-        int nodeElem = 0, wayElem = 0, relElem = 0;
+        int nodeElem = 0;
+        GraphWay graphWay = new GraphWay(ways);
         ArrayList<String> notRoad = new ArrayList<>();
         notRoad.add("footway");
         notRoad.add("pedestrian");
@@ -66,101 +57,106 @@ public class NewMain {
         dbf = DocumentBuilderFactory.newInstance();
         db = dbf.newDocumentBuilder();
         doc = db.parse(new File("src/res/nizhnekamsk.xml"));
-        analyzeXML(doc, nodeElem, wayElem, relElem, notRoad);
+        analyzeXML(doc, nodeElem, notRoad, ways);
         buildNode(doc);
         Graph graph = new Graph();
         read = new BufferedReader(new InputStreamReader(System.in));
-        double lat, lon;
+        double lat, lon, lat2, lon2;
         lat = Double.parseDouble(read.readLine());
         lon = Double.parseDouble(read.readLine());
+        lat2 = Double.parseDouble(read.readLine());
+        lon2 = Double.parseDouble(read.readLine());
         GraphNode nodeA = null;
-        for (GraphNode node:
-             nodes) {
-            graph.addNode(node);
-            node.addNextNode(nodes.iterator().next(), (int)calcDistNodes(node.getLAT(), node.getLON(),nodes.iterator().next().getLAT(),nodes.iterator().next().getLON()));
-            if(nodes.contains(lat) && nodes.contains(lon)){
-                nodeA = node;
-            }
+        GraphNode nodeZ = null;
+        for (int i = nodes.size() - 1; i > 0; i--) {
+            graph.addNode(nodes.get(i));
+            nodes.get(i).addNextNode(nodes.iterator().next(), (int)calcDistNodes(nodes.get(i).getLAT(), nodes.get(i).getLON(),nodes.get(i - 1).getLAT(),nodes.get(i - 1).getLON()));
+            if(nodes.get(i).getLAT() == lat && nodes.get(i).getLON() == lon) {
+                nodeA = nodes.get(i);
+
+            } else if(nodes.get(i).getLAT() == lat2 && nodes.get(i).getLON() == lon2)
+                nodeZ = nodes.get(i);
         }
 
-        graph = DijkstraAlgorithm.calcShortWay(graph, nodeA);
-        System.out.println(graph.getNodes());
-
-        //buildWay(doc);
+        graph = DijkstraAlgorithm.calcShortWay(graph, nodeA, nodeZ);
     }
 
-    private void analyzeXML(Document doc, int nodeElem, int wayElem, int relElem, ArrayList<String> notRoad) {
+    private void analyzeXML(Document doc, int nodeElem, ArrayList<String> notRoad, Map<Long, List<Long>> ways) {
         NodeList children = doc.getDocumentElement().getChildNodes();
+        long wayID = 0;
         for (int i = 0; i < children.getLength(); i++) {
+
             Node done = children.item(i);
+
             if (done.getNodeName().equals("node")) {
                 nodeElem = i;
-            } else if (done.getNodeName().equals("way")) {
-                wayElem = i;
-            } else if (done.getNodeName().equals("relation")) {
-                relElem = i;
             }
+
             if (done.getNodeType() == Node.ELEMENT_NODE) {
                 NamedNodeMap attributes = done.getAttributes();
                 NodeList tags = done.getChildNodes();
-                boolean t = false;
+                boolean check = false;
                 for (int j = tags.getLength() - 1; j >= 0; j--) {
+
                     Node tag = tags.item(j);
+
                     if (tag.getNodeType() == Node.ELEMENT_NODE) {
                         NamedNodeMap attrib = tag.getAttributes();
                         Node kAttrib = attrib.getNamedItem("k");
                         Node vAttrib = attrib.getNamedItem("v");
-                        if(t && tag.getNodeName().equals("nd")) refs.add(Long.parseLong(attrib.getNamedItem("ref").getNodeValue()));
+
+                        if(check && tag.getNodeName().equals("nd")) refs.add(Long.parseLong(attrib.getNamedItem("ref").getNodeValue()));
+
                         if (tag.getNodeName().equals("tag") && kAttrib.getNodeValue().equals("highway") && !notRoad.contains(vAttrib.getNodeValue())) {
-                            t = true;
-                            ways.add(new GraphWay(Long.parseLong(attributes.getNamedItem("id").getNodeValue())));
+                            check = true;
+                            wayID = Long.parseLong(attributes.getNamedItem("id").getNodeValue());
                         }
+
+                        if(j == 1) ways.put(wayID, refs);
                     }
                 }
             }
         }
-
-        setElements(nodeElem, wayElem, relElem);
-//        System.out.println(refs);
+//        System.out.println(ways);
+        setNodeElem(nodeElem);
     }
     private void buildNode(Document doc) {
         NodeList children = doc.getDocumentElement().getChildNodes();
         for (int j = 0; j < nodeElem; j++) {
+
             Node done = children.item(j);
+
             if (done.getNodeType() == Node.ELEMENT_NODE && done.getNodeName().equals("node")) {
                 // атрибуты узла
                 NamedNodeMap attributes = done.getAttributes();
                 NodeList tags = done.getChildNodes();
+
                 for (int i = 0; i < tags.getLength(); i++) {
+
                     Node tag = tags.item(i);
+
                     if (tag.getNodeType() == Node.ELEMENT_NODE ) {
                         NamedNodeMap at = tag.getAttributes();
                         long id = 0;
                         double lat = 0, lon = 0;
+                        id = Long.parseLong(attributes.getNamedItem("id").getNodeValue());
+                        lat = Double.parseDouble(attributes.getNamedItem("lat").getNodeValue());
+                        lon = Double.parseDouble(attributes.getNamedItem("lon").getNodeValue());
 
-                        try {
-                            id = Long.parseLong(attributes.getNamedItem("id").getNodeValue().replaceAll("[^0-9]", ""));
-                            lat = Double.parseDouble(attributes.getNamedItem("lat").getNodeValue().replaceAll("[^0-9]", ""));
-                            lon = Double.parseDouble(attributes.getNamedItem("lon").getNodeValue().replaceAll("[^0-9]", ""));
-                        } catch (NumberFormatException e) {
-                            e.printStackTrace();
-                        }
                         if (refs.contains(id)) {
                             nodes.add(new GraphNode(id, lat, lon));
-//                            System.out.println("id:" + id + "lat:" + lat + "lon:" + lon);
+                            //System.out.println(id + " " + lat + " " + lon);
                         }
                     }
                 }
             }
         }
-        //System.out.println(nodes);
     }
-    private void buildWay(Document doc) {
-    }
+
     private double calcDistNodes(double lat1, double lon1, double lat2, double lon2) {
         final double radEarth = 6371.009;
-        double dLAT = lat2 - lat1;
-        double dLON = lon2 - lon1;
+        double dLAT = Math.abs(lat2 - lat1);
+        double dLON = Math.abs(lon2 - lon1);
         double dO = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(dLAT/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dLON/2),2)));
         double Dist = radEarth * dO;
         return Dist;
